31e0db303b2c530f787d375746fb29124dc451a4
eiurrutia <eiurrutia@uc.cl>
2022-12-17 17:35:31 +0100

    feat: check wms service date

--- check_daily_executions.py
+++ check_daily_executions.py
@@ -43,6 +43,12 @@ def get_last_middleware_execution():
     return driver.find_element("xpath", "(//td[contains(@class,'field-syncstartdatetime nowrap')])[1]").text
 
 
+def get_wms_service_date():
+    driver.get("https://patagonia.linets.cl/admin/wms/wmsstock/")
+    driver.maximize_window()
+    return driver.find_element("xpath", "(//td[contains(@class,'field-registered_dt nowrap')])[1]").text
+
+
 def check_execution_is_today(execution_date):
     # With setLocale, we can parse the name of the months in Spanish
     locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
@@ -63,13 +69,22 @@ def parse_date_to_correct_timezone(execution_date):
     return execution_datetime.strftime('%d %B %Y %H:%M')
 
 
-def send_mail_results(result, last_execution):
+def send_mail_results(result, last_execution, wms_service_date):
     result = "Successful" if result else "Failed"
     today_date_text = dt.now().strftime('%d/%m/%Y')
     sent_from = 'keko.up9@gmail.com'
     to = ['enrique.urrutia@patagonia.com']
-    subject = 'Execution report ' + result + ' ' + today_date_text
-    body = 'Execution result for ' + today_date_text + ' was ' + result + '\nLast Execution: ' + last_execution
+    subject = 'Execution report ' + today_date_text + ' | ' + result
+    body = """
+    Online Stock execution result for today {} was {}.
+    Last Execution: {} 
+    WMS Execution: {}
+    """.format(
+        today_date_text,
+        result,
+        last_execution,
+        wms_service_date
+    )
     email_text = "\r\n".join([
       "From: " + sent_from,
       "To: enrique.urrutia@patagonia.com",
@@ -101,7 +116,8 @@ def check_correct_execution():
     last_execution_date = get_last_middleware_execution()
     send_mail_results(
         check_execution_is_today(last_execution_date),
-        parse_date_to_correct_timezone(last_execution_date)
+        parse_date_to_correct_timezone(last_execution_date),
+        get_wms_service_date()
     )
     if check_execution_is_today(last_execution_date):
         print("Ejecución correcta!")
