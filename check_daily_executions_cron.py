57cdb36d536703a1c9c6425d7c296bdc0dc576b9
eiurrutia <eiurrutia@uc.cl>
2023-01-21 13:08:20 +0100

    feat: check daily executions to cron with shopify checking

--- check_daily_executions_cron.py
+++ check_daily_executions_cron.py
@@ -1,14 +1,15 @@
-import time
+import json
 import locale
 import smtplib
+import shopify
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import SessionNotCreatedException, WebDriverException
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions \
+    import SessionNotCreatedException, WebDriverException, NoSuchElementException, TimeoutException
 from datetime import datetime as dt
 from dateutil import tz
 from local_settings import *
@@ -16,23 +17,31 @@ from local_settings import *
 
 user = MD_USER
 pwd = MD_PWD
+url = MD_URL
+shop_URL = SHOP_URL
 
 
-def set_up_driver():
+def set_up_driver(local=False):
     try:
-        #ser = Service(executable_path=r"~/root/chromedriver/chromedriver")
-        #ser = Service(executable_path=r"~/snap/bin/chromium.chromedriver")
-        ser = Service(ChromeDriverManager(version="108.0.5359.71").install())
-        op = webdriver.ChromeOptions()
-        op.add_argument('headless')
-        op.add_argument('--no-sandbox')
-        op.add_argument('--disable-dev-shm-usage')
-        op.add_argument("--remote-debugging-port=4444")
-
+        if local:
+            ser = Service(executable_path=r"C:\Users\Enrique Urrutia\Desktop\driver\chromedriver.exe")
+            op = webdriver.ChromeOptions()
+            op.add_argument('headless')
+            op.add_argument('--no-sandbox')
+            op.add_argument('--disable-dev-shm-usage')
+        else:
+            from webdriver_manager.chrome import ChromeDriverManager
+            ser = Service(ChromeDriverManager(version="108.0.5359.71").install())
+            op = webdriver.ChromeOptions()
+            op.add_argument('headless')
+            op.add_argument('--no-sandbox')
+            op.add_argument('--disable-dev-shm-usage')
+            op.add_argument("--remote-debugging-port=4444")
         return webdriver.Chrome(service=ser, options=op)
     except (SessionNotCreatedException, WebDriverException) as err:
         print(f"{dt.now()} - {err.msg}")
         return False
+
 
 def check_login_required(driver):
     try:
@@ -45,15 +54,23 @@ def check_login_required(driver):
 
 
 def get_last_middleware_execution(driver):
-    driver.get("https://patagonia.linets.cl/admin/azure_sql/erpstock/")
-    driver.maximize_window()
-    return driver.find_element("xpath", "(//td[contains(@class,'field-syncstartdatetime nowrap')])[1]").text
+    try:
+        driver.get(f"{url}/azure_sql/erpstock/")
+        driver.maximize_window()
+        return driver.find_element("xpath", "(//td[contains(@class,'field-syncstartdatetime nowrap')])[1]").text
+    except NoSuchElementException as err:
+        print(err.msg)
+        return False
 
 
 def get_wms_service_date(driver):
-    driver.get("https://patagonia.linets.cl/admin/wms/wmsstock/")
-    driver.maximize_window()
-    return driver.find_element("xpath", "(//td[contains(@class,'field-registered_dt nowrap')])[1]").text
+    try:
+        driver.get(f"{url}/wms/wmsstock/")
+        driver.maximize_window()
+        return driver.find_element("xpath", "(//td[contains(@class,'field-registered_dt nowrap')])[1]").text
+    except NoSuchElementException as err:
+        print(err.msg)
+        return False
 
 
 def check_execution_is_today(execution_date):
@@ -63,6 +80,60 @@ def check_execution_is_today(execution_date):
     execution_date = execution_date.replace("de ", "").replace("a las ", "")
     execution_datetime = dt.strptime(execution_date, '%d %B %Y %H:%M')
     return execution_datetime.date() == date_today
+
+
+def check_sku_example_with_difference(driver):
+    example_sku = None
+    example_inventory_id = None
+    # Will check max 6 pages to the inventory table.
+    try:
+        for actual_page in range(1, 6):
+            driver.get(f"{url}/md_shopify/inventory/?location_name=CD&o=-4&p={actual_page}&q=")
+            # There are max 100 rows for each page in inventory table paginated.
+            for row in range(1, 100):
+                stock_i = WebDriverWait(driver, 4).until(
+                    EC.visibility_of_element_located((By.XPATH, f"(//td[contains(@class ,'field-quantity')])[{row}]"))
+                )
+                stock_c = WebDriverWait(driver, 4).until(
+                    EC.visibility_of_element_located(
+                        (By.XPATH, f"(//td[contains(@class ,'field-new_quantity')])[{row}]"))
+                )
+                if stock_i.text != stock_c.text:
+                    example_sku = \
+                        WebDriverWait(driver, 4).until(
+                            EC.visibility_of_element_located(
+                                (By.XPATH, f"(//td[contains(@class ,'field-sku')])[{row}]"))
+                        ).text
+                    example_inventory_id = \
+                        WebDriverWait(driver, 4).until(
+                            EC.visibility_of_element_located(
+                                (By.XPATH, f"(//td[contains(@class ,'field-inventory_id')])[{row}]"))
+                        ).text
+                    break
+            if example_sku:
+                print(f"{example_inventory_id} - {example_sku}")
+                break
+        return example_inventory_id, example_sku
+    except (NoSuchElementException, TimeoutException) as err:
+        print(err)
+        return False, False
+
+
+def get_shopify_inventory_level_object(inventory_id):
+    try:
+        shopify.ShopifyResource.set_site(shop_URL)
+        return json.loads(
+            shopify.InventoryLevel.find_first(inventory_item_ids=inventory_id, location_ids=5611814967).to_json()
+        )['inventory_level']
+    except (ValueError, AttributeError) as err:
+        print(err)
+        print("Error")
+        return False
+
+
+def check_last_shopify_update_was_today(last_updated_datetime):
+    print(f"last_updated_datetime: {last_updated_datetime}")
+    return dt.fromisoformat(last_updated_datetime).date() == dt.now().date()
 
 
 def parse_date_to_correct_timezone(execution_date):
@@ -76,21 +147,35 @@ def parse_date_to_correct_timezone(execution_date):
     return execution_datetime.strftime('%d %B %Y %H:%M')
 
 
-def send_mail_results(result, last_execution, wms_service_date):
-    result = "Successful" if result else "Failed"
+def send_mail_results(last_md_execution_date, wms_service_date, shopify_loaded_date, shopify_loaded_sku):
+    if last_md_execution_date and wms_service_date and shopify_loaded_date\
+        and check_execution_is_today(last_md_execution_date)\
+            and check_last_shopify_update_was_today(shopify_loaded_date):
+        result = "Successful"
+        last_md_execution_date = parse_date_to_correct_timezone(last_md_execution_date)
+        print("Execution Successful!")
+    else:
+        result = "FAILED"
+        print("Execution Failed!!")
+
     today_date_text = dt.now().strftime('%d/%m/%Y')
     sent_from = 'keko.up9@gmail.com'
     to = ['enrique.urrutia@patagonia.com']
-    subject = 'Execution report ' + today_date_text + ' | ' + result
+    subject = result + ' | ' + 'Execution report ' + today_date_text
     body = """
+    {}
     Online Stock execution result for today {} was {}.
     Last Execution: {} 
     WMS Execution: {}
+    Shopify Loaded data: {} | {}
     """.format(
+        result,
         today_date_text,
         result,
-        last_execution,
-        wms_service_date
+        last_md_execution_date,
+        wms_service_date,
+        shopify_loaded_date,
+        shopify_loaded_sku
     )
     email_text = "\r\n".join([
       "From: " + sent_from,
@@ -116,22 +201,22 @@ def send_mail_results(result, last_execution, wms_service_date):
 def check_correct_execution():
     driver = set_up_driver()
     if not driver: return
-    driver.get("https://patagonia.linets.cl/admin/md_shopify/inventory/")
+    driver.get(f"{url}/md_shopify/inventory/")
 
     login = check_login_required(driver)
     if login:
         login.send_keys(user + Keys.TAB + pwd + Keys.TAB + Keys.ENTER)
 
-    last_execution_date = get_last_middleware_execution(driver)
+    inventory_id, sku = check_sku_example_with_difference(driver)
+    inventory_level = get_shopify_inventory_level_object(inventory_id)
+    last_shopify_update = inventory_level['updated_at'] if inventory_level else False
+
     send_mail_results(
-        check_execution_is_today(last_execution_date),
-        parse_date_to_correct_timezone(last_execution_date),
-        get_wms_service_date(driver)
+        get_last_middleware_execution(driver),
+        get_wms_service_date(driver),
+        last_shopify_update,
+        sku
     )
-    if check_execution_is_today(last_execution_date):
-        print("Ejecución correcta!")
-    else:
-        print("No hay ejecución hoy!!")
     driver.close()
 
 
